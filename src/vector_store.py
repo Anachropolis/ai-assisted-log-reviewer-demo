912c6f9d01239f8578579f64db108c78900a4af6
Anachropolis <adam.herston@gmail.com>
2026-05-16 21:36:09 -0400

    feat: added logic for vector_store module

---
+++ src/vector_store.py
@@ -0,0 +1,25 @@
+import chromadb
+from pathlib import Path
+from document_loader import DocumentLoader
+
+client = chromadb.PersistentClient(path="../data/compliance_references/compliance_db")
+collection = client.get_or_create_collection("compliance_references")
+
+class VectorStore:
+
+    def __init__(self, file_path: Path, entry: str) -> None:
+        self.client = chromadb.PersistentClient(file_path)
+        self.collection = client.get_or_create_collection(entry)
+
+    def vectorize_data(self, documents: dict) -> None:
+
+        self.collection.add(documents=documents["content"], metadatas=documents["metadata"],ids=documents["ids"])
+
+
+
+# doc = DocumentLoader()
+# doc_chunks = doc.run(Path("../data/compliance_references/logkeeping_requirements.md"))
+# vector_store = VectorStore(Path("../data/test_db"), "compliance_references")
+# vector_store.vectorize_data(doc_chunks)
+# results = vector_store.collection.query(query_texts="Tell me what log entries should include", n_results=1)
+# print(results["documents"])
