2d894f7ce192b6f07a04b1a245a80a402c6ca019
Anachropolis <adam.herston@gmail.com>
2026-05-17 17:35:29 -0400

    feat: add logic to llm_client

--- src/llm_client.py
+++ src/llm_client.py
@@ -1,1 +1,33 @@
-import openai
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+
+class LLMClient:
+
+    def __init__(self) -> None:
+        self.api_key = os.getenv("OPENAI_API_KEY")
+        self.client = OpenAI(api_key = self.api_key)
+        self.model = os.getenv("OPENAI_MODEL")
+
+
+    def query_model(self, system_prompt: str, user_prompt: str) -> str:
+        """generate json response from system + user prompt"""
+        response = self.client.chat.completions.create(
+            model = self.model,
+            messages = [{"role": "system",
+                      "content": system_prompt},
+                     {"role": "user",
+                      "content": user_prompt}])
+        return response.choices[0].message.content
+
+
+
+
+
+
+
