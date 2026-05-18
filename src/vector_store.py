0049434d73abd958bbc97572394047d387a65146
Anachropolis <adam.herston@gmail.com>
2026-05-18 19:19:03 -0400

    fix: refactor code so vector retrieval produces list of dictionaries

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,5 +1,9 @@
 import chromadb
-from pathlib import Path
+import os
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
 
 
 client = chromadb.PersistentClient(path="../data/compliance_references/compliance_db")
@@ -7,17 +11,22 @@ collection = client.get_or_create_collection("compliance_references")
 
 class VectorStore:
 
-    def __init__(self, file_path: Path, entry: str) -> None:
-        self.client = chromadb.PersistentClient(file_path)
-        self.collection = client.get_or_create_collection(entry)
+    def __init__(self) -> None:
+        self.client = chromadb.PersistentClient(os.getenv("CHROMA_PERSIST_DIR"))
+        self.collection = client.get_or_create_collection("CHROMA_COLLECTION_NAME")
 
     def vectorize_data(self, documents: dict) -> None:
 
         self.collection.add(documents=documents["content"], metadatas=documents["metadata"],ids=documents["ids"])
 
-    def retrieve_relevant_references(self, query: str, top_k: int = 3) -> object:
+    def retrieve_relevant_references(self, query: str, top_k: int = 3) -> list[dict]:
         references = self.collection.query(query_texts = query, n_results = top_k)
-        return references
+        titles = [title["Header 1"] for title in references["metadatas"][0]]
+        content = references["documents"][0]
+        response = [{"title": title, "content": content, "source_file":f"{title}.md"} for title, content in zip(titles, content)]
+
+
+        return response
 
 
 
