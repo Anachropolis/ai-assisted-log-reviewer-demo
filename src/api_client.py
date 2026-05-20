5faa9297d391dce38369245f07bd78ea2a8a54b0
Anachropolis <adam.herston@gmail.com>
2026-05-19 21:19:17 -0400

    fix: minor refactor

--- src/api_client.py
+++ src/api_client.py
@@ -11,7 +11,7 @@ class ApiClient:
         self.base_url = os.getenv("MOCK_API_BASE_URL")
 
     def fetch_operator_log(self, endpoint: str, entry: str) -> dict:
-
+        """Handles call to log API"""
         url = f"{self.base_url}/{endpoint.lstrip('/')}/{entry.lstrip('/')}"
         response = (requests.get(url, timeout=10))
         response.raise_for_status()
--- src/vector_store.py
+++ src/vector_store.py
@@ -1,7 +1,7 @@
 import chromadb
 import os
 from dotenv import load_dotenv
-import json
+
 
 load_dotenv()
 
